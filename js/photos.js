f61773394f0cae885644e8cedeb0bb13557b642b
NepPure <NepPure@users.noreply.github.com>
2021-01-07 07:46:03 +0000

    deploy: 25da608d7cba1498606ce31c80d02d3c7f2843ee

--- js/photos.js
+++ js/photos.js
@@ -8,9 +8,9 @@ openPhoto(id, url);
 lazyload();
 })(`
 <li class="item " id="紫色之心夏日" style="background-image: url('/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-65ba1d.jpg')" title="紫色之心夏日">
-  <img class="lazyload" data-src="/photos/thumbnail/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-ccf4df.jpg" src="/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-65ba1d.jpg" height="2305" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-9f36bc.jpg" src="/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-65ba1d.jpg" height="2305" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E5%A4%8F%E6%97%A5" data-target="紫色之心夏日">Open</a>
   <a class="close" href="/">Close</a>
@@ -24,9 +24,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="黑色之心" style="background-image: url('/photos/tint/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-65ba1d.jpg')" title="黑色之心">
-  <img class="lazyload" data-src="/photos/thumbnail/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-ccf4df.jpg" src="/photos/tint/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-9f36bc.jpg" src="/photos/tint/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E9%BB%91%E8%89%B2%E4%B9%8B%E5%BF%83" data-target="黑色之心">Open</a>
   <a class="close" href="/">Close</a>
@@ -43,9 +43,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="紫色之心" style="background-image: url('/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-65ba1d.jpg')" title="紫色之心">
-  <img class="lazyload" data-src="/photos/thumbnail/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-ccf4df.jpg" src="/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-9f36bc.jpg" src="/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83" data-target="紫色之心">Open</a>
   <a class="close" href="/">Close</a>
@@ -62,9 +62,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="紫色之心次时代" style="background-image: url('/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-65ba1d.jpg')" title="紫色之心次时代">
-  <img class="lazyload" data-src="/photos/thumbnail/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-ccf4df.jpg" src="/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-9f36bc.jpg" src="/photos/tint/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E7%B4%AB%E8%89%B2%E4%B9%8B%E5%BF%83%E6%AC%A1%E6%97%B6%E4%BB%A3" data-target="紫色之心次时代">Open</a>
   <a class="close" href="/">Close</a>
@@ -81,9 +81,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="美杜莎lily2" style="background-image: url('/photos/tint/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-65ba1d.jpg')" title="美杜莎Lily2">
-  <img class="lazyload" data-src="/photos/thumbnail/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-ccf4df.jpg" src="/photos/tint/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-9f36bc.jpg" src="/photos/tint/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily2-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E7%BE%8E%E6%9D%9C%E8%8E%8Elily2" data-target="美杜莎lily2">Open</a>
   <a class="close" href="/">Close</a>
@@ -100,9 +100,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="美杜莎lily" style="background-image: url('/photos/tint/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-65ba1d.jpg')" title="美杜莎Lily">
-  <img class="lazyload" data-src="/photos/thumbnail/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-ccf4df.jpg" src="/photos/tint/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-9f36bc.jpg" src="/photos/tint/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E7%BE%8E%E6%9D%9C%E8%8E%8ELily-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E7%BE%8E%E6%9D%9C%E8%8E%8Elily" data-target="美杜莎lily">Open</a>
   <a class="close" href="/">Close</a>
@@ -119,9 +119,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="光" style="background-image: url('/photos/tint/%E5%85%89-65ba1d.jpg')" title="光">
-  <img class="lazyload" data-src="/photos/thumbnail/%E5%85%89-ccf4df.jpg" src="/photos/tint/%E5%85%89-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E5%85%89-9f36bc.jpg" src="/photos/tint/%E5%85%89-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E5%85%89-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E5%85%89-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E5%85%89" data-target="光">Open</a>
   <a class="close" href="/">Close</a>
@@ -138,9 +138,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="焰" style="background-image: url('/photos/tint/%E7%84%B0-65ba1d.jpg')" title="焰">
-  <img class="lazyload" data-src="/photos/thumbnail/%E7%84%B0-ccf4df.jpg" src="/photos/tint/%E7%84%B0-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E7%84%B0-9f36bc.jpg" src="/photos/tint/%E7%84%B0-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E7%84%B0-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E7%84%B0-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E7%84%B0" data-target="焰">Open</a>
   <a class="close" href="/">Close</a>
@@ -157,9 +157,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="幼贞-2-2" style="background-image: url('/photos/tint/%E5%B9%BC%E8%B4%9E-2-2-65ba1d.jpg')" title="幼贞-2-2">
-  <img class="lazyload" data-src="/photos/thumbnail/%E5%B9%BC%E8%B4%9E-2-2-ccf4df.jpg" src="/photos/tint/%E5%B9%BC%E8%B4%9E-2-2-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E5%B9%BC%E8%B4%9E-2-2-9f36bc.jpg" src="/photos/tint/%E5%B9%BC%E8%B4%9E-2-2-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E5%B9%BC%E8%B4%9E-2-2-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E5%B9%BC%E8%B4%9E-2-2-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E5%B9%BC%E8%B4%9E-2-2" data-target="幼贞-2-2">Open</a>
   <a class="close" href="/">Close</a>
@@ -176,9 +176,9 @@ lazyload();
 	</ul> -->
 </li>
 <li class="item " id="幼贞-1-4" style="background-image: url('/photos/tint/%E5%B9%BC%E8%B4%9E-1-4-65ba1d.jpg')" title="幼贞-1-4">
-  <img class="lazyload" data-src="/photos/thumbnail/%E5%B9%BC%E8%B4%9E-1-4-ccf4df.jpg" src="/photos/tint/%E5%B9%BC%E8%B4%9E-1-4-65ba1d.jpg" height="2731" width="4096" />
+  <img class="lazyload" data-src="/photos/thumbnail/%E5%B9%BC%E8%B4%9E-1-4-9f36bc.jpg" src="/photos/tint/%E5%B9%BC%E8%B4%9E-1-4-65ba1d.jpg" height="2731" width="4096" />
   <span class="full">
-    <span style="background-image: url('/photos/large/%E5%B9%BC%E8%B4%9E-1-4-686656.jpg')"></span>
+    <span style="background-image: url('/photos/large/%E5%B9%BC%E8%B4%9E-1-4-78c28e.jpg')"></span>
   </span>
   <a class="open" href="/%E5%B9%BC%E8%B4%9E-1-4" data-target="幼贞-1-4">Open</a>
   <a class="close" href="/">Close</a>
