f5906df36baac8f76aece5908e43714a5704f307
野原小牛 <oyyz@vip.qq.com>
2021-01-07 16:34:26 +0800

    remove rand

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -5,8 +5,7 @@ module Jekyll
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-        rand(1..100000)
-        # EXIFR::JPEG.new(photo.path).date_time.to_s
+        EXIFR::JPEG.new(photo.path).date_time.to_s
       end.reverse
     end
   end
